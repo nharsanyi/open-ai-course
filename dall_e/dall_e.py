c319eefa5a7bc0b08d15e8ca0545391f3dfa50d0
Nikolett Harsanyi <nikolett.harsanyi@trinitymirror.com>
2024-01-17 13:03:31 +0000

    Add image edit and variation

--- dall_e/dall_e.py
+++ dall_e/dall_e.py
@@ -27,24 +27,51 @@ def main():
     else:
         save_image_content(img, response_format)
 
+    variation_url = create_img_variation(f"{image_directory_name}/astronaut.png")
+    download_image(variation_url, f"{image_directory_name}/astronaut_variation.png")
+    edit_url = create_img_edit(img_path=f"{image_directory_name}/great_wave.png",
+                               mask_path=f"{image_directory_name}/great_wave_mask.png",
+                               prompt="A pink flamingo floaty")
+    download_image(edit_url, f"{image_directory_name}/great_wave_edit.png")
+
+
+def create_img_edit(img_path, mask_path, prompt):
+    resp = openai.Image.create_edit(image=open(img_path, "rb"),
+                                    mask=open(mask_path, "rb"),
+                                    prompt=prompt,
+                                    n=1,
+                                    size="1024x1024")
+    url = resp["data"][0]["url"]
+    print(url)
+    return url
+
+
+def create_img_variation(img_path: str):
+    resp = openai.Image.create_variation(
+        image=open(img_path, "rb"),
+        n=1,
+        size="1024x1024"
+    )
+    return resp['data'][0]['url']
+
 
 def generate_image(prompt: str, response_format="url"):
     res = openai.Image.create(
         prompt=prompt,
         size="1024x1024",
         n=1,
-        response_format=response_format # or b64_json
+        response_format=response_format  # or b64_json
     )
     return res["data"][0][response_format]
 
 
-def download_image(image_url):
+def download_image(image_url, local_path=f"{image_directory_name}/astronaut.png"):
     image_content = requests.get(image_url).content
-    save_image_content(image_content, "url")
+    save_image_content(image_content, "url", local_path)
 
 
-def save_image_content(image_content, image_format):
-    with open(f"{image_directory_name}/astronaut.png", "wb") as f:
+def save_image_content(image_content, image_format, local_path):
+    with open(local_path, "wb") as f:
         if image_format == "b64_json":
             decoded_image = base64.b64decode(image_content)
             f.write(decoded_image)
